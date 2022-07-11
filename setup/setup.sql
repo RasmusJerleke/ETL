d8b3ecde481bbcfcfa1bf37f59874b21e773b7f1
Nezcobar <hajt@live.se>
2022-07-11 11:56:09 +0200

    updated setup.sql

--- setup/setup.sql
+++ setup/setup.sql
@@ -1,12 +1,19 @@
 DROP SCHEMA IF EXISTS modelled;
 DROP SCHEMA IF EXISTS staged;
 DROP SCHEMA IF EXISTS cleansed;
-DROP DATABASE IF EXISTS databasename; 
+DROP DATABASE IF EXISTS archprep; 
+DROP USER IF EXISTS etl_user;
+
+\c postgres postgres
+
+create user etl_user with encrypted password '123';
 
 CREATE DATABASE archprep;
--- GRANT ALL PRIVILEGES ON DATABASE archprep to <usr>
--- ALTER DATABASE archprep OWNER TO <usr>;
-\c archprep
+GRANT ALL PRIVILEGES ON DATABASE archprep to etl_user;
+ALTER DATABASE archprep OWNER TO etl_user;
+
+\c archprep etl_user
+
 CREATE SCHEMA cleansed;
 CREATE SCHEMA staged;
 CREATE SCHEMA modelled;

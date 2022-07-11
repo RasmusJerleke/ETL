ba52e6953969fa61fb916ea5e4f60f70ae30244d
Nezcobar <hajt@live.se>
2022-07-11 16:36:26 +0200

    6.22.2.3,4,5 mm

---
+++ raw_to_harmonized/raw_to_harmonized.py
@@ -0,0 +1,23 @@
+import json
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import globals
+
+# 2. Write a function that reads the JSON files from data/testing/raw and transforms it to a 
+# list of tuples, then returns this list together with the keys from the JSON object.
+def read_json():
+    with open(globals.RAW_DATA_PATH, 'r') as f:
+        data = json.load(f)
+    return [x for x in data.items()], data.keys()
+
+# tup, keys = read_json()
+# print(tup, keys)
+
+
+
+# write another function that takes as inputs a list of tuples, a list of strings, and a filepath. 
+# The function should write the list as a JSON object to file.
+def write_to_file(tuple_list, strings, filepath=globals.HARMONIZED_DATA_PATH):
+    dict = {t[0] : t[1] for t in tuple_list}
+    with open(filepath, 'w') as f:
+        json.dump(dict, f, indent=4)
